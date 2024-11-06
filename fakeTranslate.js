f1c246b9d3e195eea4a5a1809ac8355612891a44
abdilakhatovak <kanykei.abdilakhatova@alatoo.edu.kg>
2024-11-06 23:19:08 +0600

    fakeTranslate

---
+++ fakeTranslate.js
@@ -0,0 +1,37 @@
+const dictionary = {
+    "hello": "здравствуйте",
+    "world": "мир",
+    "friend": "друг",
+    "good": "хорошо",
+    "day": "день",
+    "night": "ночь",
+    "love": "любовь",
+    "peace": "мир",
+    "cat": "кот",
+    "dog": "собака",
+    "water": "вода",
+    "fire": "огонь",
+    "earth": "земля",
+    "wind": "ветер",
+    "sun": "солнце",
+    "moon": "луна",
+    "tree": "дерево",
+    "flower": "цветок",
+    "book": "книга",
+    "computer": "компьютер"
+};
+
+function fakeTranslate(word) {
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+            const translation = dictionary[word.toLowerCase()];
+            if (translation) {
+                resolve(translation); 
+            } else {
+                reject(`Слово "${word}" не найден`); 
+            }
+        }, 1000);
+    });
+}
+
